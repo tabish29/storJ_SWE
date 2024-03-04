fc59aa71487c701021f046b354463731045fb7d2
davidederosa <davide.derosa@studio.unibo.it>
2024-03-04 15:55:22 +0100

    modifiche docker, con docker compose che tira su entrambi i container collegati

--- db/sql/tables.sql
+++ db/sql/tables.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS utente(
 	id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
-    password VARCHAR(50) NOT NULL
+    password VARCHAR(50) NOT NULL,
+    statopagamento BOOLEAN NOT NULL
 );
