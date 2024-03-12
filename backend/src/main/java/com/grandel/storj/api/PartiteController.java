b27254bf8e8baf8253483013bb097a11272dc186
davidederosa <davide.derosa@studio.unibo.it>
2024-03-12 18:53:33 +0100

    modifica iniziale controller vari

--- backend/src/main/java/com/grandel/storj/api/PartiteController.java
+++ backend/src/main/java/com/grandel/storj/api/PartiteController.java
@@ -2,9 +2,11 @@ package com.grandel.storj.api;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import storj.api.PartiteApi;
+import storj.model.Partita;
 
 @Slf4j
 @Controller
@@ -12,4 +14,20 @@ import storj.api.PartiteApi;
 @RequestMapping("/api/v1")
 public class PartiteController implements PartiteApi {
 
+
+    public ResponseEntity<Partita> getPartitaById(Long idPartita) {
+        return PartiteApi.super.getPartitaById(idPartita);
+    }
+
+    public ResponseEntity<Partita> postPartita(Partita partita) {
+        return PartiteApi.super.postPartita(partita);
+    }
+
+    public ResponseEntity<Partita> putPartita(Long idPartita, Partita partita) {
+        return PartiteApi.super.putPartita(idPartita, partita);
+    }
+
+    public ResponseEntity<Void> deletePartita(Long idPartita) {
+        return PartiteApi.super.deletePartita(idPartita);
+    }
 }
--- backend/src/main/java/com/grandel/storj/api/ScelteController.java
+++ backend/src/main/java/com/grandel/storj/api/ScelteController.java
@@ -2,9 +2,12 @@ package com.grandel.storj.api;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import storj.api.ScelteApi;
+import storj.model.Indovinello;
+import storj.model.Multipla;
 
 @Slf4j
 @Controller
@@ -12,4 +15,36 @@ import storj.api.ScelteApi;
 @RequestMapping("/api/v1")
 public class ScelteController implements ScelteApi {
 
+
+    public ResponseEntity<Indovinello> getSceltaIndovinelloById(Long idScelta) {
+        return ScelteApi.super.getSceltaIndovinelloById(idScelta);
+    }
+
+    public ResponseEntity<Multipla> getSceltaMultiplaById(Long idScelta) {
+        return ScelteApi.super.getSceltaMultiplaById(idScelta);
+    }
+
+    public ResponseEntity<Indovinello> postSceltaIndovinello(Indovinello indovinello) {
+        return ScelteApi.super.postSceltaIndovinello(indovinello);
+    }
+
+    public ResponseEntity<Multipla> postSceltaMultipla(Multipla multipla) {
+        return ScelteApi.super.postSceltaMultipla(multipla);
+    }
+
+    public ResponseEntity<Indovinello> putSceltaIndovinello(Long idScelta, Indovinello indovinello) {
+        return ScelteApi.super.putSceltaIndovinello(idScelta, indovinello);
+    }
+
+    public ResponseEntity<Multipla> putSceltaMultipla(Long idScelta, Multipla multipla) {
+        return ScelteApi.super.putSceltaMultipla(idScelta, multipla);
+    }
+
+    public ResponseEntity<Void> deleteSceltaIndovinello(Long idScelta) {
+        return ScelteApi.super.deleteSceltaIndovinello(idScelta);
+    }
+
+    public ResponseEntity<Void> deleteSceltaMultipla(Long idScelta) {
+        return ScelteApi.super.deleteSceltaMultipla(idScelta);
+    }
 }
