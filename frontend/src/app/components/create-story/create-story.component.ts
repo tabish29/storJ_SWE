00896f58f4d2295513cef70ff9e42893bd2f9f8b
tabish29 <tabish13295@gmail.com>
2024-03-21 20:09:46 +0100

    correzione della visualizzazione del drop nel box degli scenari

--- frontend/src/app/components/create-story/create-story.component.ts
+++ frontend/src/app/components/create-story/create-story.component.ts
@@ -13,71 +13,64 @@ import { storyObjectService } from '../../services/story-object.service';
   templateUrl: './create-story.component.html',
   styleUrl: './create-story.component.css'
 })
-export class CreateStoryComponent {
+export class CreateStoryComponent implements OnInit {
 
   //Da modificare fare in modo che vengano visualizzate solo le storie dell'utente loggato
-  scenarios: scenario[]=[];
+  scenarios: scenario[] = [];
   storyId!: number;
-  dropMap: Map<number, string | undefined>=new Map();
+  dropMap: Map<number, string | undefined> = new Map();
   dropName!: string;
+  countTEST = 0;
+  countMetodiLoad=0;
+  isloadCompleted!: boolean;
+
 
   constructor(private scenarioService: ScenarioService, private dropService: DropService, private storyObjectService: storyObjectService, private router: Router, private localStorageService: LocalStorageService) { }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
     // Recupera l'ID della storia corrente dal localStorage
     const currentStory = this.localStorageService.getItem('currentStory');
     this.storyId = currentStory.id;
-    this.loadScenarios();
+    await this.loadScenarios();
+    await this.loadDrop(this.scenarios);
+
   }
 
-  loadScenarios(): void {
-    this.scenarioService.getScenarioByStoryId(this.storyId).subscribe(
-      (scenarios: scenario[]) => {
+
+  async loadScenarios(): Promise<void> {
+    try {
+      const scenarios = await (this.scenarioService.getScenarioByStoryId(this.storyId)).toPromise();
+      this.countMetodiLoad++;
+      if (scenarios) {
         this.scenarios = scenarios;
-        console.log("lunghezza dell'array scenari scenario "+scenarios.length);
-        this.loadDrop(scenarios);
-      },
-      error => {
-        console.log('Errore nel caricamento dei scenari');
       }
-    );
+    } catch (error) {
+      console.log('Errore nel caricamento dei scenari', error);
+    }
   }
 
   async loadDrop(scenarios: scenario[]): Promise<void> {
     for (const scenario of scenarios) {
-        try {
-            const drop = await this.dropService.getDropByScenarioId(scenario.id).toPromise();
-            if (drop) {
-                try {
-                    const storyObject = await this.storyObjectService.getStoryObject(drop.id_oggetto).toPromise();
-                    if(storyObject){
-                    this.dropMap.set(scenario.id, storyObject.nome);
-                  }
-                    
-                } catch (error) {
-                    console.error("Errore nel caricamento del nome dell'oggetto da drop", error);
-                }
-            } else {
-                this.dropMap.set(scenario.id, "Nessun Drop");
+      try {
+        const drop = await this.dropService.getDropByScenarioId(scenario.id).toPromise();
+        this.countMetodiLoad++;
+        if (drop) {
+          try {
+            const storyObject = await this.storyObjectService.getStoryObject(drop.id_oggetto).toPromise();
+            if (storyObject) {
+              this.dropMap.set(scenario.id, storyObject.nome);
             }
-        } catch (error) {
-            console.error(`Errore nel caricamento dei drop per lo scenario ${scenario.id}`, error);
+
+          } catch (error) {
+            console.log("Errore nel caricamento del nome dell'oggetto da drop: " + error);
+          }
+        } else {
+          this.dropMap.set(scenario.id, "Nessun Drop");
         }
+      } catch (error) {
+        console.log("Errore nel caricamento dei drop per lo scenario " + scenario.id + ": " + error);
+      }
     }
-}
-
-  scenarioHasDrop(idScenario: number): boolean {
-      // Per visualizzare la Map come array di [key, value]
-      console.log(Object.fromEntries(this.dropMap));
-   
-    if(this.dropMap.has(idScenario)){
-      console.log(idScenario+"esiste nella mappa");
-
-    }else{
-      console.log(idScenario+"non esiste nella mappa");
-    }
-    return this.dropMap.has(idScenario);
-
   }
 
   removeScenario(scenarioId: number): void {
@@ -93,7 +86,7 @@ export class CreateStoryComponent {
     } else if (type === 'INDOVINELLO') {
       this.router.navigateByUrl('/singlechoice');
     } else {
-      console.warn('Tipo di risposta non riconosciuto:', type);
+      console.log('Tipo di risposta non riconosciuto: ' + type);
     }
   }
 
