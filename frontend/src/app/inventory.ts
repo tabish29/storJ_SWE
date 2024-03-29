4d982c627e860ba8fa1bedfaf0e0996e1e3b6f9f
Canghiari04 <matteo.canghiari@gmail.com>
2024-03-29 11:41:51 +0100

    creazione classi partita ed inventario

---
+++ frontend/src/app/inventory.ts
@@ -0,0 +1,7 @@
+export class inventory {
+    constructor(
+       public id: number,
+       public id_partita: number,
+       public id_oggetto: number
+    ) { }
+}
---
+++ frontend/src/app/match.ts
@@ -0,0 +1,8 @@
+export class match {
+    constructor(
+       public id: number,
+       public id_storia: number,
+       public id_utente: number,
+       public id_scenario_corrente: number
+    ) { }
+}
