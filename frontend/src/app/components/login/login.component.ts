2d31354e4e7716340e82b04d3136b947810bdaa6
Canghiari04 <matteo.canghiari@gmail.com>
2024-03-30 18:36:38 +0100

    commentato codice

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -13,7 +13,6 @@ import { AuthService } from '../../services/auth.service';
 export class LoginComponent {
   username: string = '';
   password: string = '';
-
   
   constructor(private http: HttpClient,private userService: UserService,private authService:AuthService,private router: Router) { }
   
@@ -22,10 +21,8 @@ export class LoginComponent {
       (response: user) => {
         if(response.password === this.password){
           this.authService.setAuthStatus(true);
-          this.userService.changeUser(response); 
-          
-          //redirect to pagina storJPage!
-          this.router.navigateByUrl('/storJPage'); // Usa il Router per il redirect
+          this.userService.changeUser(response);           
+          this.router.navigateByUrl('/storJPage');
         }else{
           this.authService.setAuthStatus(false);
           alert("Credenziali di accesso errate!");
--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -25,20 +25,15 @@ export class NavbarComponent implements OnInit, OnDestroy {
   scenarioId!: number;
   isInTextEditMode!: boolean;
 
-
   constructor(private router: Router, private authService: AuthService, private userService: UserService, private storyService: StoryService, private singleChoiceService: SingleChoiceService, private localStorageService: LocalStorageService) {
-    // Imposta l'URL corrente a currentUrl
     this.currentUrl = router.url;
   }
 
   ngOnDestroy(): void {
     this.userSub.unsubscribe();
-
   }
 
   ngOnInit() {
-
-    //Per ottenere l'URL della pagina in cui si trova l'applicazione
     this.subscription.add(
       this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe(event => {
         if (event instanceof NavigationEnd) {
@@ -48,39 +43,29 @@ export class NavbarComponent implements OnInit, OnDestroy {
     );
 
     this.userSub = this.userService.currentUser.subscribe((user: user | null) => {
-      if (user) {
+      if(user) {
         this.showPaymentButton = !user.statoPagamento;
       } else {
-        // Se user è null, imposta showPaymentButton a false
         this.showPaymentButton = false;
       }
     }, error => {
       console.log('error:', error);
     });
 
-    //imposto il valore dell'id dello scenario corrente per poter utilizzare il metodo isSingleChoiceCreated
     const currentscenario = this.localStorageService.getItem('currentScenario');
-    if (currentscenario) {
+    if(currentscenario) {
       this.scenarioId = currentscenario.id;
-
-      //imposto il valore di isSingleChoiceCreated
       this.isSingleChoiceCreated(this.scenarioId);
-
     }
 
-    //per capire se mostare o meno i bottoni Crea nella navbar
     this.isInTextEditMode = this.storyService.isStoryCompleted();
-
-
     console.log("valore dell'url current: " + this.currentUrl);
   }
 
-  // Metodo per verificare se l'utente è autenticato
   isAuthenticated(): boolean {
     return this.authService.isAuthenticated();
   }
 
-  // Metodo per effettuare il logout
   logout(): void {
     this.authService.logout();
     this.router.navigate(['/login']); // Reindirizza l'utente alla pagina di login dopo il logout
@@ -91,10 +76,10 @@ export class NavbarComponent implements OnInit, OnDestroy {
   }
 
   isSingleChoiceCreated(scenarioID: number): void {
-    if (scenarioID) {
+    if(scenarioID) {
       this.singleChoiceService.getSingleChoiceByScenarioId(scenarioID).subscribe({
         next: (response) => {
-          if (response) {
+          if(response) {
             this.singleChoiceService.setIsChoiceCreated(true);
           } else {
             this.singleChoiceService.setIsChoiceCreated(false);
@@ -109,7 +94,6 @@ export class NavbarComponent implements OnInit, OnDestroy {
       console.error("Non c'è un id dello scenario corrente");
     }
   }
-
 
   saveStory(): void {
     this.storyService.saveStory().subscribe(
@@ -140,7 +124,6 @@ export class NavbarComponent implements OnInit, OnDestroy {
       this.router.navigateByUrl('/homeStories');
   }
 
-  //(da decidere)Se tenere la freccia in tutte le pagine oppure solo in quelle presenti nel metodo
   goBack(page: string): void {
     switch (page) {
       case '/handlerPlaypage':
