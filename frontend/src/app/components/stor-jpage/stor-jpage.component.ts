dd1eb15405818c7e3a8272d98693d000de6b857a
tabish29 <tabish13295@gmail.com>
2024-04-03 20:34:22 +0200

    modifica al messaggio di benvenuto

--- frontend/src/app/components/stor-jpage/stor-jpage.component.ts
+++ frontend/src/app/components/stor-jpage/stor-jpage.component.ts
@@ -1,8 +1,28 @@
 import { Component } from '@angular/core';
+import { user } from '../../user';
 
 @Component({
   selector: 'app-stor-jpage',
   templateUrl: './stor-jpage.component.html',
   styleUrl: './stor-jpage.component.css'
 })
-export class StorJPageComponent { }
+export class StorJPageComponent {
+  currentUser: user | null = null; // Potrebbe non esserci un utente loggato
+  username: String | undefined = '';
+
+  ngOnInit() {
+    this.loadCurrentUsername();
+  }
+
+  loadCurrentUsername(): void {
+    const userJson = localStorage.getItem('currentUser');
+    if (userJson) {
+      this.currentUser = JSON.parse(userJson);
+      this.username = this.currentUser?.username;
+    }
+  }
+
+
+
+
+}
